ad85614078735d4d415da6b748cb1ba8f3920c9f
Ding-Dongen <pavado.pvd@gmail.com>
2024-11-07 09:04:09 +0100

    fix

--- uppgift-react/src/components/Header.jsx
+++ uppgift-react/src/components/Header.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 import SiliconLogo from '../assets/images/icons/logo-navbar.svg'
 import DarkModeSwitch from './DarkModeSwitch'
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false)
+
+    const togglMenu = () => {
+      setShowMenu(!showMenu)
+    }
+
+
   return (
     <section className="page-1">
             <div id="container-1" className="nav">
                 
                 <Link id="logo" to="/"><img src={SiliconLogo} alt="Logo" /><span>Silicon</span></Link>
 
-                <nav id="main-menu" className="navbar" aria-label="Main Navigation">
+                  <nav id="main-menu" className={`${showMenu ? 'navbar' : ''}`} aria-label="Main Navigation">
                     <a className="nav-link" href="#container-3">Features</a>
                     <a className="nav-link" href="./contact">Contact</a>
                   </nav>
@@ -22,7 +29,7 @@ const Header = () => {
                   <span>Sign in / up</span>
                 </a>
 
-                <button className="btn-mobile">
+                <button className="btn-mobile" onClick={togglMenu}>
                   <i className="fa-solid fa-bars"></i>
                 </button>
 
